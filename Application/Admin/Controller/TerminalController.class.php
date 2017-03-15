98687127b0222338cc2a5b4580f84abd092199bb
graduatezhu <graduatezhu@163.com>
2017-03-15 14:08:11 +0800

    修改终端管理页面样式

--- Application/Admin/Controller/TerminalController.class.php
+++ Application/Admin/Controller/TerminalController.class.php
@@ -4,48 +4,64 @@ use Think\Controller;
 class TerminalController extends Controller {
     //关于我们页面的展示
     public function index(){
-        
+/*header*/        
         // 在header显示系统当前时间
         $date= date("Y年m月d日" ,time()).' 星期'.getWeek(time());
         
         // 在header显示系统当前登录的用户名
 		$user=mb_substr($_SESSION['admininfo']['username'],0,4).'***';
+
+/*状态栏*/		
         $ob=M('charge_pile');
         $field='*';
         $pile['totalQty']=$ob->field($field)->count(); // 桩总数
         $pile['dcQty']=$ob->where('type=0')->count(); // 直流桩数量
         $pile['acQty']=$ob->where('type=1')->count(); // 交流桩数量
-        
+      
         // 今日充电次数
         $times=$ob->field('sum(times_today) as times')->find(); 
         $pile['times']=$times['times']; 
         
-        // 状态,注意status值为枚举类型
+        // 注意status值为枚举类型
         $pile['occupyQty']=$ob->where("status='0'")->count(); // 工作中    
         $pile['idleQty']=$ob->where("status='1'")->count(); // 空闲
         $pile['faultQty']=$ob->where("status='2'")->count(); // 故障
         
+        
+/*列表分页*/
+        if(!empty($_POST['txtStation'])){
+            $_POST['txtStation']=trim($_POST['txtStation']);
+            $where['name']=array('like',"%{$_POST['txtStation']}%");
+        }
+        
         $ob_station=M('charge_station');
         $field='id,name,ac_num+dc_num as pile_total,electricity_history,times_today';
-        $re_station=$ob_station->field($field)->select();
-
+          
+        //取得总条数
+        $count=$ob_station->where($where)->count();
+        //根据总条数实例化page类
+        $page= new \Think\Page($count,8);
+        //分页显示输出
+        $show= $page->show();
+        $list = $ob_station->field($field)->where($where)->limit($page->firstRow,$page->listRows)->select();
+        
+        //每个电站中桩的状态统计
         $ob_pile=M('charge_pile');
-        //每个电站中各桩的状态统计
-        foreach ($re_station as $k=>$v) {
-            $stationID=$re_station[$k]['id'];
-            $re_station[$k]['sequence']=$k+1;
-            $re_station[$k]['pile_occupy']=$ob_pile->where("station_id=$stationID AND status='0'")->count();
-            $re_station[$k]['pile_idle']=$ob_pile->where("station_id=$stationID AND status='1'")->count();
-            $re_station[$k]['pile_fault']=$ob_pile->where("station_id=$stationID AND status='2'")->count();
+        foreach ($list as $k=>$v) {
+            $stationID=$list[$k]['id'];
+            $list[$k]['sequence']=$k+1;
+            $list[$k]['pile_occupy']=$ob_pile->where("station_id=$stationID AND status='0'")->count();
+            $list[$k]['pile_idle']=$ob_pile->where("station_id=$stationID AND status='1'")->count();
+            $list[$k]['pile_fault']=$ob_pile->where("station_id=$stationID AND status='2'")->count();
             // $pile_status[$k]=$ob_pile->where($where)->field("count(status) as num")->group("status")->select();
         }
-
         
         $this->assign('curdate',$date);
         $this->assign('curuser',$user);
         $this->assign('pilesinfo',$pile);
 
-        $this->assign('lists',$re_station);
+        $this->assign('lists',$list);
+        $this->assign("show",$show);
         $this->display();
     }
     
---
+++ Public/js/my.js
@@ -0,0 +1,21 @@
+﻿$(function(){
+	// 导出
+    $('#export').click(function () {
+    	 var selectValue=$("#paytype option:selected").val();  //获取Select选择值
+    	 var textValue = $("#ordeno").val();//获取text值
+    	alert("select值："+selectValue+" text值:"+textValue);
+        location.href = 'http://localhost/zuche/admin/ExportOrders/?ordernumber='+textValue+'&paytype='+selectValue;
+    })
+    
+    // 终端管理-查找电站
+    $("#stationSearch").click(function(){
+           var txtStation=$("#txtStation").val();
+           var durl='__APP__/Admin/Terminal/index';
+           $.post(durl,{txtStation:txtStation});
+    })
+   
+
+
+
+})
+
