4d9768bc0485449b8c5153105be2a6060eca0a0a
zws <15836117462@163.com>
2017-03-20 15:43:40 +0800

    财务界面初版

---
+++ Application/Admin/Controller/FinancialController.class.php
@@ -0,0 +1,28 @@
+<?php
+namespace Admin\Controller;
+use Think\Controller;
+class FinancialController extends Controller {
+    //关于我们页面的展示
+    public function index(){
+        /*header*/
+        // 在header显示系统当前时间
+        $date= date("Y年m月d日" ,time()).' 星期'.getWeek(time());
+
+        // 在header显示系统当前登录的用户名
+        $user=mb_substr($_SESSION['admininfo']['username'],0,4).'**';
+
+        //订单数据
+        $count=M('charge_order')->where()->count();
+        $page= new \Think\Page($count,5);
+        $show=$page->show();
+        $order=M('charge_order')->order('id desc')->where()->limit($page->firstRow,$page->listRows)->select();
+
+
+        $this->assign('curdate',$date);
+        $this->assign('curuser',$user);
+        $this->assign('show',$show);
+        $this->assign('lists',$order);
+        $this->display();
+    }
+    
+}
