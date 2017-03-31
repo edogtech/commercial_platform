7c336476f932f3302f9f25c17f9b97a7b13b19b1
zws <15836117462@163.com>
2017-03-31 15:11:01 +0800

    短信api加入

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -12,6 +12,10 @@ class IndexController extends Controller {
     }
 
     public function find_password2(){
+        if(!isset($_POST['phone']) || !isset($_POST['code'])){
+            $this->redirect('Index/find_password1');
+        }
+
         $phone=trim($_POST['phone']);
         $code=intval($_POST['code']);
 
@@ -32,7 +36,19 @@ class IndexController extends Controller {
     }
 
     public function find_password3(){
+        if(!isset($_POST['id']) || !isset($_POST['code'])){
+            $this->redirect('Index/find_password1');
+        }
+
+
+
+        $id=$_POST['id'];
+        $this->assign('id',$id);
         $this->display();
+    }
+
+    public function modify_password(){
+
     }
 
     public function register1(){
@@ -86,6 +102,31 @@ class IndexController extends Controller {
             $this->redirect('Index/index', array('error' => 3));
         }
 
+
+    }
+
+    public function  send_text(){
+        require_once  '/PUBLIC/jsms-api-php-client-master/src/JSMS.php';
+
+        $appKey = '459d9bee0bac542534a6fd57';
+        $masterSecret = '35e380846e272cfe8ca678ee';
+        $phone = '15836117462';
+
+
+        $code=mt_rand(111111,999999);
+        $_SESSION['phone_code']=['time'=>time(),'code'=>$code];
+        //这里的 $temp_id 和 $temp_para 的值需要到 "极光控制台 -> 短信验证码 -> 模板管理" 里面获取
+        $temp_id = '37524';
+        $temp_para = ['code' => $code];
+
+        $client = new \JiGuang\JSMS($appKey, $masterSecret,[ 'ssl_verify' => false ]);
+        $response = $client->sendMessage($phone, $temp_id, $temp_para);
+
+        if($response['http_code']==200){
+            echo 'success';
+        }else{
+            echo 'fail';
+        }
 
     }
 
