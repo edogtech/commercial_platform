64a4aac85f5572b20776977ae583a96ee1f0c828
graduatezhu <graduatezhu@163.com>
2017-04-05 14:16:59 +0800

    更改shell调用路径

--- Application/Admin/Common/function.php
+++ Application/Admin/Common/function.php
@@ -21,7 +21,7 @@ function getWeek($date)
  * @author ZXD
  */
 function execShell($val1,$val2,$val3){
-    shell_exec("/www/shell/cron.php  $val1 $val2 $val3"); // 把三个起始时间送给crontab定时调用改价命令
+    shell_exec("/www/shell/cron.sh  $val1 $val2 $val3"); // 把三个起始时间送给crontab定时调用改价命令
 }
 
 /*-------------------电桩控制命令BEGIN------------------*/
