ee432bcf892fc0d2fad8c4737a5a89ce00adab59
zws <15836117462@163.com>
2017-04-20 17:07:38 +0800

    经营管理1

--- Application/Admin/Controller/FinancialController.class.php
+++ Application/Admin/Controller/FinancialController.class.php
@@ -146,14 +146,7 @@ class FinancialController extends Controller {
         }
 
         $objWriter = new \PHPExcel_Writer_Excel2007($objPHPExcel);
-//        $today=date('Y-m-d',time());
-//        if(!file_exists("./Public/saveExcel/{$today}")){
-//            mkdir("./Public/saveExcel/{$today}");
-//        }
-//        if(!file_exists("./Public/saveExcel")){
-//            mkdir("./Public/saveExcel");
-//        }
-//        $path="./Public/saveExcel/{$today}/{$_SESSION['admininfo']['uname']}".time().".xlsx";
+
         $excelName='统计表';
         header ( "Content-Type: application/force-download" );
         header ( "Content-Type: application/octet-stream" );
