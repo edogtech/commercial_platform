c09751537323237e7ae8e0310542e6db3e8fffb7
zws <15836117462@163.com>
2017-05-03 15:01:12 +0800

    经营管理1.8 全部完成

--- Application/Admin/Controller/OperateController.class.php
+++ Application/Admin/Controller/OperateController.class.php
@@ -588,4 +588,93 @@ class OperateController extends Controller{
 
     }
 
+    public function invoice_excel(){
+        include './Public/phpexcel/Classes/PHPExcel.php';
+        include './Public/phpexcel/Classes/PHPExcel/Writer/Excel2007.php';
+        set_time_limit(0);
+
+        //接收筛选参数
+        //添加检索条件
+        if(!empty(trim($_GET['search']))){
+            $map['phone']=array('eq',$_GET['search']);
+        }
+        
+        if(!empty(trim($_GET['start'])) && empty(trim($_GET['end']))){
+            $map['addtime']=array('gt',strtotime($_GET['start']));
+        }
+
+        if(empty(trim($_GET['start'])) && !empty(trim($_GET['end']))){
+            $map['addtime']=array('lt',strtotime($_GET['end']));
+        }
+
+        if(!empty(trim($_GET['start'])) && !empty(trim($_GET['end']))){
+            $starttime=strtotime($_GET['start']);
+            $endtime=strtotime($_GET['end']);
+            $map['addtime']=array('exp',"between $starttime and $endtime");
+        }
+
+        $map['mid']=array('eq',$_GET['mid']);
+
+        //先查询数据
+        $res=M('invoice_control')->where($map)->select();
+
+        if(empty($res)){
+            echo '没有数据可导出,请查看您是否有成本管理录入或检查您的筛选条件';
+            exit;
+        }
+        //设置F列(订单号)为文本格式
+        $objPHPExcel=new \PHPExcel();//新建一个excel文件类
+
+        //设置表头
+        $objPHPExcel->getActiveSheet()->setCellValue('A1','序号');
+        $objPHPExcel->getActiveSheet()->setCellValue('B1','流水号');
+        $objPHPExcel->getActiveSheet()->setCellValue('C1','联系人电话');
+        $objPHPExcel->getActiveSheet()->setCellValue('D1','联系人姓名');
+        $objPHPExcel->getActiveSheet()->setCellValue('E1','发票类型');
+        $objPHPExcel->getActiveSheet()->setCellValue('F1','发票价格');
+        $objPHPExcel->getActiveSheet()->setCellValue('G1','申请日期');
+        $objPHPExcel->getActiveSheet()->setCellValue('H1','操作员');
+
+
+        //设置每列宽度
+        $objPHPExcel->getActiveSheet()->getColumnDimension('B')->setWidth(30);
+        $objPHPExcel->getActiveSheet()->getColumnDimension('C')->setWidth(12);
+        $objPHPExcel->getActiveSheet()->getColumnDimension('D')->setWidth(12);
+        $objPHPExcel->getActiveSheet()->getColumnDimension('E')->setWidth(12);
+        $objPHPExcel->getActiveSheet()->getColumnDimension('F')->setWidth(20);
+        $objPHPExcel->getActiveSheet()->getColumnDimension('G')->setWidth(20);
+        $objPHPExcel->getActiveSheet()->getColumnDimension('H')->setWidth(15);
+        $line=2;
+        foreach($res as $k=>$v){
+            $objPHPExcel->getActiveSheet()->setCellValue('A'.$line,$v['id']);
+            $objPHPExcel->getActiveSheet()->setCellValue('B'.$line,$v['order_number']);
+            $objPHPExcel->getActiveSheet()->setCellValue('C'.$line,'tel:'.$v['phone']);
+            $objPHPExcel->getActiveSheet()->setCellValue('D'.$line,$v['customer']);
+            switch ($v['type']) {
+                case 1:
+                    $objPHPExcel->getActiveSheet()->setCellValue('E'.$line, '充电发票');
+                    break;
+            }
+            $objPHPExcel->getActiveSheet()->setCellValue('F'.$line,$v['price']);
+            $objPHPExcel->getActiveSheet()->setCellValue('G'.$line,date('Y-m-d H:i:s',$v['addtime']));
+            $objPHPExcel->getActiveSheet()->setCellValue('H'.$line,$v['operator']);
+
+            $line++;
+        }
+
+        $objWriter = new \PHPExcel_Writer_Excel2007($objPHPExcel);
+
+        $excelName='统计表';
+        header ( "Content-Type: application/force-download" );
+        header ( "Content-Type: application/octet-stream" );
+        header ( "Content-Type: application/download" );
+        header ( "Content-Disposition: attachment; filename=" . $excelName . ".xlsx" );
+        header ( "Content-Transfer-Encoding: binary" );
+        header ( "Cache-Control: must-revalidate, post-check=0, pre-check=0" );
+        header ( "Pragma: no-cache" );
+
+        $objWriter->save('php://output');
+
+    }
+
 }
